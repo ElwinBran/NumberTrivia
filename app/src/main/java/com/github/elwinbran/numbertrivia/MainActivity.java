6221c41162246bfa26cb14c9548f36f835de733f
ElwinBran <elwinbran@gmail.com>
2018-11-23 07:57:12 +0100

    Added some basics that will are part of highest behaviour.

--- app/src/main/java/com/github/elwinbran/numbertrivia/MainActivity.java
+++ app/src/main/java/com/github/elwinbran/numbertrivia/MainActivity.java
@@ -4,26 +4,39 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final List<NumberTrivia> triviaList = new ArrayList<>();
+        RecyclerView triviaView = null;
+        triviaView.setLayoutManager(new LinearLayoutManager(this));
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view)
+            {
+                Integer randomNumber = new Random().nextInt();
+                //api call
+                NumberTrivia newTrivia = new NumberTrivia(null, null);
+                triviaList.add(newTrivia);
             }
         });
     }
